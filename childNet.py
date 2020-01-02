8f700996120e1eb67864356685227ff92f99e75b
RualPerez <RualPerez@AutoML>
2020-01-02 21:24:15 +0100

    Submit final project

--- childNet.py
+++ childNet.py
@@ -5,82 +5,75 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+activation_functions = {
+    'Sigmoid': nn.Sigmoid(),
+    'Tanh': nn.Tanh(),
+    'ReLU': nn.ReLU(),
+    'LeakyReLU': nn.LeakyReLU(),
+    'Linear': nn.Identity()
+}
 
-def create_dataset():
+def create_dataset(p_val=0.1, p_test=0.2):
     import numpy as np
     import sklearn.datasets
 
     # Generate a dataset and plot it
     np.random.seed(0)
-    num_samples = 500
+    num_samples = 1000
 
-    X, y = sklearn.datasets.make_moons(num_samples, noise=0.20)
-
+    X, y = sklearn.datasets.make_moons(num_samples, noise=0.2)
+    
+    train_end = int(len(X)*(1-p_val-p_test))
+    val_end = int(len(X)*(1-p_test))
+    
     # define train, validation, and test sets
-    X_tr = X[:100].astype('float32')
-    X_val = X[100:400].astype('float32')
-    X_te = X[400:].astype('float32')
+    X_tr = X[:train_end]
+    X_val = X[train_end:val_end]
+    X_te = X[val_end:]
 
     # and labels
-    y_tr = y[:100].astype('int32')
-    y_val = y[100:400].astype('int32')
-    y_te = y[400:].astype('int32')
+    y_tr = y[:train_end]
+    y_val = y[train_end:val_end]
+    y_te = y[val_end:]
 
     #plt.scatter(X_tr[:,0], X_tr[:,1], s=40, c=y_tr, cmap=plt.cm.Spectral)
     return X_tr, y_tr, X_val, y_val
 
 class Net(nn.Module):
 
-    def __init__(self, actions, num_features, num_output, layer_limit): 
+    def __init__(self, layers, num_features, num_classes, layer_limit): 
         super(Net, self).__init__()
       
+        #if hid_units is None or len(hid_units) == 0:
+        #    raise Exception('You must specify at least one action!')
 
-        max_layers = 5
-        if max_layers < layer_limit:
-            print('Maximum layers that ChildNet accepts is '.format(max_layers))
-            raise 
-
-        from copy import deepcopy
-        hidd_units_layers = deepcopy(actions)
-        hidd_units_layers[-1] = num_output
-        self.nb_layers = len(hidd_units_layers)
-
-        while len(hidd_units_layers)<max_layers:
-            hidd_units_layers.append(64)
-
-        self.l_1 = nn.Linear(in_features=num_features, 
-                          out_features=hidd_units_layers[0],
-                          bias=True)
+        layers_added = []
         
-        self.l_2 = nn.Linear(in_features=hidd_units_layers[0], 
-                          out_features=hidd_units_layers[1],
-                          bias=True)
+        max_layers = 7
+        if max_layers < layer_limit:
+            raise Exception('Maximum layers that ChildNet accepts is '.format(max_layers))
 
-        self.l_3 = nn.Linear(in_features=hidd_units_layers[1], 
-                          out_features=hidd_units_layers[2],
-                          bias=True)
+        hidd_unit_prev = num_features
+        
+        for i,layer in enumerate(layers):
+            if isinstance(layer, int):
+                layer_to_add = nn.Linear( in_features=hidd_unit_prev, out_features=layer)
+                layers_added.append(layer_to_add)
+                hidd_unit_prev = layer
+            elif layer == 'EOS':
+                break
+            else:
+                layers_added.append(activation_functions[layer])
+                
+        #last layer must contain 2 out_features (2 classes)
+        layers_added.append(nn.Linear(in_features=hidd_unit_prev, out_features=num_classes))
 
-        self.l_4 = nn.Linear(in_features=hidd_units_layers[2], 
-                          out_features=hidd_units_layers[3],
-                          bias=True)
-
-        self.l_5 = nn.Linear(in_features=hidd_units_layers[3], 
-                          out_features=hidd_units_layers[4],
-                          bias=True)
-
-        self.layers = [self.l_1, self.l_2, self.l_3, self.l_4, self.l_5]
+        self.layers = nn.Sequential(*layers_added)
+        self.optimizer = optim.Adam(self.parameters(), lr=1e-2)
     
     def forward(self, x):
-        for i,layer in enumerate(self.layers):
-            if i < self.nb_layers-1:
-                #print(layer)
-                x = F.relu(layer(x))
-            elif i < self.nb_layers:
-                x = layer(x)   
-                #print('here', layer)       
-        #assert False
-        return x 
-
+        return self.layers(x)
+    
 def accuracy(ys, ts):
     # making a one-hot encoded vector of correct (1) and incorrect (0) predictions
     correct_prediction = torch.eq(ts.long(), torch.max(ys, 1)[1])
@@ -97,20 +90,23 @@ class ChildNet():
         self.criterion = nn.CrossEntropyLoss()
 
         X_tr, y_tr, X_val, y_val = create_dataset()
-        self.X_tr = X_tr
-        self.y_tr = y_tr
-        self.X_val = X_val
-        self.y_val = y_val
+        self.X_tr = X_tr.astype('float32')
+        self.y_tr = y_tr.astype('float32')
+        self.X_val = X_val.astype('float32')
+        self.y_val = y_val.astype('float32')
         
         self.num_features = X_tr.shape[-1]
-        self.num_output = 2
+        self.num_classes = 2
         self.layer_limit = layer_limit
 
-    def compute_reward(self, hidd_units_layers, num_epochs):
+    def compute_reward(self, layers, num_epochs):
         # store loss and accuracy for information
         train_losses = []
-        net = Net(hidd_units_layers, self.num_features, self.num_output, self.layer_limit)
-        optimizer = optim.Adam(net.parameters(), lr=0.001)
+        val_accuracies = []
+        patience = 10
+        
+        net = Net(layers, self.num_features, self.num_classes, self.layer_limit)
+        #print(net)
         max_val_acc = 0
         
         # get training input and expected output as torch Variables and make sure type is correct
@@ -121,37 +117,53 @@ class ChildNet():
         val_input = Variable(torch.from_numpy(self.X_val))
         val_targets = Variable(torch.from_numpy(self.y_val))
 
+        patient_count = 0
         # training loop
         for e in range(num_epochs):
 
-            # zeroize accumulated gradients in parameters
-            optimizer.zero_grad()
             # predict by running forward pass
             tr_output = net(tr_input)
             # compute cross entropy loss
+            #tr_loss = F.cross_entropy(tr_output, tr_targets.type(torch.LongTensor)) 
             tr_loss = self.criterion(tr_output.float(), tr_targets.long())
-            train_losses.append(tr_loss.data.numpy())
+            # zeroize accumulated gradients in parameters
+            net.optimizer.zero_grad()
+            
             # compute gradients given loss
             tr_loss.backward()
             #print(net.l_1.weight.grad)
             # update the parameters given the computed gradients
-            optimizer.step()
+            net.optimizer.step()
             
-            if num_epochs - e < 6:
-                #AFTER TRAINING
+            train_losses.append(tr_loss.data.numpy())
+        
+            #AFTER TRAINING
 
-                # predict with validation input
-                val_output = net(val_input)
-                # compute loss and accuracy
-                val_loss = self.criterion(val_output.float(), val_targets.long())
-                val_acc = accuracy(val_output, val_targets)
-                val_acc = float(val_acc.numpy())
-                
-                if val_acc > max_val_acc:
-                	max_val_acc = val_acc
-                
-                #reset weights
-                net.apply(weight_reset)
+            # predict with validation input
+            val_output = net(val_input)
+            val_output = torch.argmax(F.softmax(val_output, dim=-1), dim=-1)
             
-        #return float(val_acc.numpy()), num_epochs, val_loss, train_losses, val_output, val_targets #-float(val_loss.detach().numpy()) 
-        return max_val_acc**3 #-float(val_loss.detach().numpy()) 
+            # compute loss and accuracy
+            #val_loss = self.criterion(val_output.float(), val_targets.long())
+            val_acc = torch.mean(torch.eq(val_output, val_targets.type(torch.LongTensor)).type(torch.FloatTensor))
+            
+            #accuracy(val_output, val_targets)
+            val_acc = float(val_acc.numpy())
+            val_accuracies.append(val_acc)
+            
+            
+            #early-stopping
+            if max_val_acc > val_acc:
+                patient_count += 1             
+                if patient_count == patience:
+                    break
+            else:
+                max_val_acc = val_acc
+                patient_count = 0
+            
+
+
+        #reset weights
+        net.apply(weight_reset)
+            
+        return val_acc#max_val_acc#**3 #-float(val_loss.detach().numpy()) 
